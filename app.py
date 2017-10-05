ed65aa9b817eb097dd56abe9baa0f563db15311c
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-10-04 21:35:08 -0400

    done

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ HW07 -- Do I Know You?
 2017-10-05
 '''
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 import os
 
 app = Flask(__name__)
@@ -39,9 +39,10 @@ def login():
 @app.route("/logout", methods=["POST"])
 def logout():
     #verifies if logout button was clicked
-    if(request.form["logout"] == "logout"):
-        session.pop("username") #removes username from session, logging out
-        return redirect(url_for("/")) #returns to login page
+    print request.form
+    if(request.form["log"] == "logout"):
+        session.pop("username", None) #removes username from session, logging out
+        return redirect("/") #returns to login page
 
 if __name__ == "__main__":
     app.debug = True
